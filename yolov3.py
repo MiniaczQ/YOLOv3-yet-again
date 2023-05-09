0010ffec5768101f357a4290c73af1634891927a
Dawid Sygocki <24943032+Dove6@users.noreply.github.com~>
2023-05-09 23:38:25 +0200

    Generate expected outputs from annotations
    (untested and partially uncommented)

--- yolov3.py
+++ yolov3.py
@@ -1,9 +1,11 @@
 from model_loader import load_model_from_file
 from modules import YOLOv3
 import lightning as pl
+from sys import float_info
 import torch
 from torch import nn, Tensor
 import torch.nn.functional as F
+from torchvision.ops import box_iou
 
 from processing import process_into_aabbs, process_with_nms, process_prediction
 
@@ -41,36 +43,77 @@ class YoloV3Module(pl.LightningModule):
         return self.model(x)
 
     # based on: https://towardsdatascience.com/calculating-loss-of-yolo-v3-layer-8878bfaaf1ff
+    # based on: https://github.com/v-iashin/WebsiteYOLO/blob/master/darknet.py#L237
     def _process_annotations(
-        self, annotations: torch.Tensor, output_size: torch.Size
+        self,
+        outputs: torch.Tensor,
+        annotations: torch.Tensor,
+        anchors: torch.Tensor,
+        ignore_thresh: float = 0.7,
     ) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor, torch.Tensor]:
+        output_size = outputs.shape
         grid_size = output_size[1]
         assert output_size[1] == output_size[2]
         obj_mask = torch.zeros(output_size[:-1], dtype=torch.bool, device=self.device)
         class_mask = torch.zeros(output_size[:-1], device=self.device)
         iou_scores = torch.zeros(output_size[:-1], device=self.device)
-
-        processed_bbox = annotations[1:] * grid_size
-        processed_xy = processed_bbox[:2]
-
-        # TODO: WIP
-
-        noobj_mask = ~obj_mask
         processed = torch.zeros(output_size, device=self.device)
+
+        def wh_iou(wh1: torch.Tensor, wh2: torch.Tensor) -> torch.Tensor:
+            intersection = torch.min(wh1[0], wh2[0]) * torch.min(wh1[1], wh2[1])
+            union = wh1[0] * wh1[1] + wh2[0] * wh2[1] - intersection
+            return intersection / (union + float_info.epsilon)
+
+        image_batch_ids, class_ids = annotations[..., :2].long().t()
+        processed_bbox = annotations[..., 2:] * grid_size
+
+        p_xy, p_wh = processed_bbox[..., :2], processed_bbox[..., 2:]
+        p_i, p_j = p_xy.long().t()
+
+        wh_iou_per_anchor = wh_iou(anchors, p_wh)
+        _, best_anchors = wh_iou_per_anchor.max(0)
+        obj_mask[image_batch_ids, best_anchors, p_j, p_i] = 1
+        noobj_mask = ~obj_mask
+        for i, gt_anchor_iou in enumerate(wh_iou_per_anchor.t()):
+            noobj_mask[
+                wh_iou_per_anchor[i], gt_anchor_iou > ignore_thresh, p_j[i], p_i[i]
+            ] = 0
+
+        # TODO: why p_j before p_i? inverting coordinates?
+        processed[image_batch_ids, best_anchors, p_j, p_i, :2] = p_xy - p_xy.floor()
+        processed[image_batch_ids, best_anchors, p_j, p_i, 2:4] = torch.log(
+            p_wh / anchors[best_anchors] + float_info.epsilon
+        )
+        processed[..., 4] = obj_mask.float()
+        processed[image_batch_ids, best_anchors, p_j, p_i, 5 + class_ids] = 1
+
+        outputs_bbox = outputs[image_batch_ids, best_anchors, p_j, p_i, :4]
+        outputs_probabilities = outputs[image_batch_ids, best_anchors, p_j, p_i, 5:]
+        _, outputs_class_ids = torch.max(outputs_probabilities, dim=-1)
+        class_mask[image_batch_ids, best_anchors, p_j, p_i] = (
+            outputs_class_ids == class_ids
+        ).float()
+
+        iou_scores[image_batch_ids, best_anchors, p_j, p_i] = box_iou(
+            outputs_bbox + outputs_bbox[..., :2],
+            processed_bbox + processed_bbox[..., :2],
+        ).diag()
+
         return (processed, obj_mask, noobj_mask, class_mask, iou_scores)
 
     # based on: https://towardsdatascience.com/calculating-loss-of-yolo-v3-layer-8878bfaaf1ff
     def _compute_loss(
-        self, predicted: torch.Tensor, expected: torch.Tensor
+        self,
+        predicted: torch.Tensor,
+        expected: torch.Tensor,
+        mask_obj: torch.Tensor,
+        mask_noobj: torch.Tensor,
     ) -> torch.Tensor:
-        mask_obj = torch.ones_like(
-            predicted[..., 0], dtype=torch.bool, device=self.device
-        )
         loss_x, loss_y, loss_w, loss_h = (
             F.mse_loss(predicted[..., i][mask_obj], expected[..., i][mask_obj])
             for i in range(4)
         )
-        # magic numbers - honestly I don't know what they mean yet
+        # TODO: magic numbers - honestly I don't know what they mean yet
         scale_loss_obj = [1, 100]  # magic numbers for obj and noobj
         loss_obj = torch.tensor(
             [
@@ -78,7 +121,7 @@ class YoloV3Module(pl.LightningModule):
                 * F.binary_cross_entropy(
                     predicted[..., 4][mask], expected[..., 4][mask]
                 )
-                for mask, scale in zip((mask_obj, ~mask_obj), scale_loss_obj)
+                for mask, scale in zip((mask_obj, mask_noobj), scale_loss_obj)
             ],
             device=self.device,
         ).sum()
@@ -89,23 +132,34 @@ class YoloV3Module(pl.LightningModule):
 
     def training_step(self, batch: torch.Tensor, batch_idx):
         # input: transformed image
-        # annotations: annotations (class_id, x [0..1], y [0..1], w [0..1], h [0..1])
+        # annotations: annotation_batch_size * (image_id, class_id, x [0..1], y [0..1], w [0..1], h [0..1])
         #     where (x, y): center, (w, h): size, [0..1] wrt width or height
+        #     and image_id identifies images within a single image batch
         input, annotations = batch
         loss, processed_annotations = {}, {}
         heads = ("x52", "x26", "x13")
-        # outputs: Size([3, grid_size, grid_size, (4 + 1 + num_classes)]) for each head
+        # outputs: Size([batch_size, anchors, grid_size, grid_size, (bbox + obj + num_classes)]) for each head
+        #     where anchors: 3, bbox: 4, obj: 1, num_classes: 2
         outputs = dict(zip(heads, self(input)))
-        for head, head_outputs in outputs.items():
+        for i, (head, head_outputs) in enumerate(outputs.items()):
             # using sigmoid to ensure [0..1] for x, y, objectness and per-class probabilities
             trunc_idx = [0, 1, 4] + list(range(5, 5 + self.num_classes))
-            head_outputs[:, :, :, trunc_idx] = torch.sigmoid(
-                head_outputs[:, :, :, trunc_idx]
+            head_outputs[..., trunc_idx] = torch.sigmoid(head_outputs[..., trunc_idx])
+            (
+                processed_annotations[head],
+                mask_obj,
+                mask_noobj,
+                mask_class,
+                iou_scores,
+            ) = self._process_annotations(
+                head_outputs, annotations, self.anchors.view(3, 3, 2)[i]
             )
-            processed_annotations[head] = self._process_annotations(
-                annotations, head_outputs.shape
+            loss[head] = self._compute_loss(
+                head_outputs,
+                processed_annotations[head],
+                mask_obj,
+                mask_noobj,
             )
-            loss[head] = self._compute_loss(head_outputs, processed_annotations[head])
         return loss, outputs, processed_annotations
 
     def validation_step(self, batch, batch_idx):
