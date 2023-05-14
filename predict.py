a1999e570342276a18da9b69514d0bcbcfec1dad
Dawid Sygocki <24943032+Dove6@users.noreply.github.com~>
2023-05-14 14:39:25 +0200

    Add dataset dir and sample count args to user scripts

--- predict.py
+++ predict.py
@@ -14,7 +14,13 @@ from output_processing.display import output_results
 OUTPUT_DIR = datetime.now().strftime("./detection_results/pklot/%Y-%m-%d_%H-%M-%S")
 
 
-def main(loaded_checkpoint_path: Union[str, IO], debug=False, show_num=0):
+def main(
+    loaded_checkpoint_path: Union[str, IO],
+    dataset_root="./data/PKLot",
+    sample_count=16,
+    show_num=0,
+    debug=False,
+):
     if debug:
         torch.manual_seed(0)
     torch.set_float32_matmul_precision("medium")
@@ -23,13 +29,13 @@ def main(loaded_checkpoint_path: Union[str, IO], debug=False, show_num=0):
         accelerator="auto" if not debug else "cpu",
         devices=1,
         logger=False,
-        limit_predict_batches=32 if not debug else 1,
+        limit_predict_batches=sample_count if not debug else 1,
     )
 
     model = YoloV3Module.load_from_checkpoint(loaded_checkpoint_path)
     results = trainer.predict(
         model,
-        datamodule=DataModule(cpu_count() if not debug else 0),
+        datamodule=DataModule(dataset_root, cpu_count() if not debug else 0),
     )
 
     with torch.no_grad():
@@ -40,6 +46,8 @@ if __name__ == "__main__":
     parser = ArgumentParser()
     parser.add_argument("loaded_checkpoint", type=FileType("rb"))
     parser.add_argument("-d", "--debug", action="store_true")
+    parser.add_argument("--dataset", type=str, default="./data/PKLot")
+    parser.add_argument("--samples", type=int, default=16)
     parser.add_argument("-n", "--shownum", type=int, default=0)
     args = parser.parse_args()
-    main(args.loaded_checkpoint, args.debug, args.shownum)
+    main(args.loaded_checkpoint, args.dataset, args.samples, args.shownum, args.debug)
